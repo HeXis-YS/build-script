721dfcbd5113d8a88117c3da1b285114b64ddee4
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2024-06-19 21:03:36 +0800

    Update build.sh

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export CGO_ENABLED=0
+export GOEXPERIMENT=newinliner
+export GOGC=off
+export GOMEMLIMIT=4GiB
+
+git clone https://github.com/XTLS/Xray-core
+
+pushd Xray-core
+tag="$(git describe --abbrev=0 --tags)"
+git checkout $tag
+
+XGCFLAGS="-B"
+XLDFLAGS="-X github.com/xtls/xray-core/core.build=$(git describe --tags) -s -w -buildid="
+
+echo "Building Xray-core $tag"
+for goamd64 in v2 v3
+do
+    export GOAMD64=${goamd64}
+    GOOS=linux go build -o ../xray_linux_amd64_${goamd64} -trimpath -gcflags=all="$XGCFLAGS" -ldflags=all="$XLDFLAGS" ./main
+    GOOS=windows go build -o ../xray_windows_amd64_${goamd64}.exe -trimpath -gcflags=all="$XGCFLAGS" -ldflags="$XLDFLAGS" ./main
+done
+popd
