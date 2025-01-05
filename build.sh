9d51043dcee6172139aa4942a4483d7baf64c2b8
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2025-01-06 03:51:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 export PATH="$HOME/go/bin:$PATH"
 export CGO_ENABLED=0
-export GOEXPERIMENT=newinliner
+# export GOEXPERIMENT=newinliner
 export GOGC=off
 export GOMEMLIMIT=4GiB
 
 cd $HOME
 
-git clone https://github.com/fatedier/frp
+git clone https://github.com/fatedier/frp -b master
 
 pushd frp
 tag="$(git describe --abbrev=0 --tags)"
 git checkout $tag
 
-XGCFLAGS="-B"
-XLDFLAGS="-s -w -buildid="
+GCFLAGS="-B"
+LDFLAGS="-s -w -buildid="
 
 echo "Building frp $tag"
 for goamd64 in v2 v3
 do
     export GOAMD64=${goamd64}
-    GOOS=linux go build -o ../frps_linux_amd64_${goamd64} -trimpath -gcflags=all="$XGCFLAGS" -ldflags=all="$XLDFLAGS" ./cmd/frps
-    GOOS=windows go build -o ../frpc_windows_amd64_${goamd64}.exe -trimpath -gcflags=all="$XGCFLAGS" -ldflags="$XLDFLAGS" ./cmd/frpc
+    GOOS=linux go build -o ../frps_linux_amd64_${goamd64} -trimpath -gcflags=all="$GCFLAGS" -ldflags="$LDFLAGS" ./cmd/frps
+    GOOS=linux go build -o ../frpc_linux_amd64_${goamd64} -trimpath -gcflags=all="$GCFLAGS" -ldflags="$LDFLAGS" ./cmd/frpc
+    GOOS=windows go build -o ../frpc_windows_amd64_${goamd64}.exe -trimpath -gcflags=all="$GCFLAGS" -ldflags="$LDFLAGS" ./cmd/frpc
 done
 popd
