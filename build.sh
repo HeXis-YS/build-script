6da575ccc136c11ee5b1ab26aa24b6f9303e436d
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2025-01-06 03:53:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH="$HOME/go/bin:$PATH"
 export CGO_ENABLED=0
-export GOEXPERIMENT=newinliner
+# export GOEXPERIMENT=newinliner
 export GOGC=off
 export GOMEMLIMIT=4GiB
 
@@ -13,14 +13,14 @@ pushd Xray-core
 tag="$(git describe --abbrev=0 --tags)"
 git checkout $tag
 
-XGCFLAGS="-B"
-XLDFLAGS="-X github.com/xtls/xray-core/core.build=$(git describe --tags) -s -w -buildid="
+GCFLAGS="-B"
+LDFLAGS="-X github.com/xtls/xray-core/core.build=$(git describe --tags) -s -w -buildid="
 
 echo "Building Xray-core $tag"
 for goamd64 in v2 v3
 do
     export GOAMD64=${goamd64}
-    GOOS=linux go build -o ../xray_linux_amd64_${goamd64} -trimpath -gcflags=all="$XGCFLAGS" -ldflags=all="$XLDFLAGS" ./main
-    GOOS=windows go build -o ../xray_windows_amd64_${goamd64}.exe -trimpath -gcflags=all="$XGCFLAGS" -ldflags="$XLDFLAGS" ./main
+    GOOS=linux go build -o ../xray_linux_amd64_${goamd64} -trimpath -gcflags=all="$GCFLAGS" -ldflags="$LDFLAGS" ./main
+    GOOS=windows go build -o ../xray_windows_amd64_${goamd64}.exe -trimpath -gcflags=all="$GCFLAGS" -ldflags="$LDFLAGS" ./main
 done
 popd
